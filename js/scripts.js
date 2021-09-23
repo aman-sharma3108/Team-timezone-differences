7a0b5d540b7d944bfc9da3c5bf2c7eff32adea9c
Khai Tran <80138112+MauTanKhaiTran@users.noreply.github.com>
2021-09-23 15:14:03 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,20 @@
 "use strict"
 
 
+
+
 function init(){
     if (document.getElementById("new_sale") != null){
-        var MaxInputs = 20;
+        var MaxInputs = 10;
         var InputsWrapper = $("#input_wrapper");
         var AddButton = document.getElementById("AddMoreFileBox");
-        var FieldCount=1;
         var x = InputsWrapper.length;
         
         
         AddButton.onclick = function (){
             if(x < MaxInputs)
             {
-                FieldCount++;
-                $(InputsWrapper).append('<p class="row"><label for="productname">Product: </label><input type="text" placeholder="Product ID" class="products" name="products[]"> x <input type="text" placeholder="Quantity" class="quantities" name="quantities[]" maxlength="4" size="4"/> = <input type="text" placeholder="Subtotal" class="subtotal" onchange="calc()" name="subtotal[]" size="6"/> <button type="button" class="removeclass">x</button></p>');
-                //$(InputsWrapper).append('<p class="row"><label for="productname">Product: </label><input type="text" placeholder="Product ID" id="product'+FieldCount+'" name="product'+FieldCount+'"> x <input type="text" placeholder="Quantity" id="quantity'+x+'" name="quantity'+x+'" maxlength="4" size="4"/> = <input type="text" readonly="readonly" placeholder="Subtotal" id="subtotal'+x+'" name="subtotal'+x+'" size="6"/> <button type="button" class="removeclass">x</button></p>');
+                $(InputsWrapper).append('<p class="row"><label for="productname">Product: </label><input type="text" placeholder="Product ID" class="products" name="products[]"> x <input type="text" placeholder="Quantity" class="quantities" name="quantities[]" maxlength="4" size="4"/> = <input type="text" placeholder="Subtotal" class="subtotals" onchange="calc()" name="subtotals[]" size="6"/> <button type="button" class="removeclass">x</button></p>');
                 x++;
             }
         return false;
@@ -32,16 +31,8 @@ function init(){
         });
 
         
-    };
-
-    var dropdowns = document.getElementById("dropbtn");
-    dropdowns.onclick = function (){
-      document.getElementById("myDropdown").classList.toggle("show");
-    };
+    }
 }
-  
-  // Close the dropdown menu if the user clicks outside of it
- 
 
 function calc(){
     var InputsWrapper = $("#input_wrapper");
@@ -54,13 +45,4 @@ function calc(){
     });
     document.getElementById("tprice").value=total;
 }
-
-window.addEventListener("click", function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-    }
-});
 window.onload = init;
