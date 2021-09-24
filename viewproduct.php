1a4727c210889b8f58fbdace7562b3d47cad0ddc
vankongv <90138834+vankongv@users.noreply.github.com>
2021-09-24 23:25:30 +0700

    Update viewproduct.php

--- viewproduct.php
+++ viewproduct.php
@@ -45,6 +45,49 @@
         
         mysqli_close($connection);
     ?>
+
+    <button type="button" onclick="tableToCSV()">
+        Download
+    </button>
+
+    <script type="text/javascript">
+        function tableToCSV() {
+            var csv_data = [];
+            var rows = document.getElementsByTagName('tr');
+
+            for (var i = 0; i < rows.length; i++) {
+                var cols = rows[i].querySelectorAll('td,th');
+                var csvrow = [];
+
+                for (var j = 0; j < cols.length; j++) {
+                    csvrow.push(cols[j].innerHTML);
+                }
+                csv_data.push(csvrow.join(","));
+            }
+            csv_data = csv_data.join('\n');
+            downloadCSVFile(csv_data);
+        }
+ 
+        function downloadCSVFile(csv_data) {
+
+            CSVFile = new Blob([csv_data], {
+                type: "text/csv"
+            });
+ 
+            var temp_link = document.createElement('a');
+ 
+            temp_link.download = "Products.csv";
+            var url = window.URL.createObjectURL(CSVFile);
+            temp_link.href = url;
+ 
+            temp_link.style.display = "none";
+            document.body.appendChild(temp_link);
+
+            temp_link.click();
+            document.body.removeChild(temp_link);
+        }
+    </script>
+
     <?php
         include "./includes/footer.inc";
     ?>
